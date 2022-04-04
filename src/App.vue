e01ef7c37ff9bcff01cec2d9089466e4cec7ba48
Helmi <93758176+Meeno14@users.noreply.github.com>
2022-04-04 11:57:17 +0700

    render using vue router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-  <HelloWorld/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
